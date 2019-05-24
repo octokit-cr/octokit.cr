02271bef9f3d270caa0d533555af83aa65d505dd
Chris Watson <chris@watzon.me>
2019-05-23 20:48:36 -0700

    Do some spec configuration

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,12 +1,22 @@
-require "json"
-require "base64"
 require "spec"
 require "webmock"
+require "hi8"
 require "../src/octokit"
 
 # Webmock.stub(:any, )
 
-def b64toJSON(string : String, model = JSON::Any)
-  decoded = Base64.decode_string(string)
-  model.from_json(decoded)
+HI8.configure do |config|
+  config.on_playback do |recorder, request, response|
+    uri = URI.parse(request.uri)
+    query = uri.query.to_s
+    uri.query = ""
+    req_headers = recorder.headers_from_hash(request.headers)
+    req_query = HTTP::Params.try(&.parse(query)).to_h
+    res_status = response.status.to_i
+    res_headers = recorder.headers_from_hash(response.headers)
+
+    ::WebMock.stub(request.method, uri.to_s)
+      .with(query: req_query)
+      .to_return(status: res_status, body: response.body, headers: res_headers)
+  end
 end
